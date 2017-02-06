977eb868b01ef839fbb011991e2c7c58c8f3b27f
Kim Min-Ho <minho1a@hanmail.net>
2017-02-06 07:43:24 +0000

    Add a casper module to find problem solved.

---
+++ casper/find.js
@@ -0,0 +1,26 @@
+var casper = require('casper').create({verbose: true});
+var id = casper.cli.args[0];
+
+casper.start('https://www.acmicpc.net/status/?user_id=' + id + '&language_id=-1&result_id=4', function() {
+	casper.then(function() {
+		var table = casper.evaluate(function() {
+			return {
+				sourceNumber:
+					[].map.call(__utils__.findAll('#status-table tr td:nth-child(1)'), function(element) {
+						return element.innerHTML;
+					}),
+
+				problemNumber:
+					[].map.call(__utils__.findAll('#status-table tr td:nth-child(3) a'), function(element) {
+						return element.innerHTML;
+					})
+			}
+		});
+
+		casper.echo(JSON.stringify(table, null, 4));
+	});
+});
+
+casper.run(function() {
+	casper.exit();
+});
