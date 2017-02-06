447d6655df65d7dbaf3af61ddf22266fb48629bd
Kim Min-Ho <minho1a@hanmail.net>
2017-02-06 03:54:35 +0000

    Add git module.

---
+++ git.js
@@ -0,0 +1,35 @@
+var exec = require('child_process').exec;
+
+exports.clone = function(remoteUrl, successClone) {
+	exec('git clone ' + remoteUrl, function(error, stdout, stderr) {
+		successClone(error, stdout, stderr);
+	});
+}
+
+exports.add = function(problemNumber, repoPath, successAdd) {
+	exec('git add ' + problemNumber, {cwd: repoPath}, function(error, stdout, stderr) {
+		successAdd(error, stdout, stderr);
+	});
+}
+
+exports.commit = function(commitMessage, repoPath, successCommit) {
+	exec('git commit -m "' + commitMessage + '"', {cwd: repoPath}, function(error, stdout, stderr) {
+		successCommit(error, stdout, stderr);
+	});
+}
+
+exports.push = function(remoteUrl, repoPath, successPush) {
+	exec('git push ' + remoteUrl + ' master', {cwd: repoPath}, function(error, stdout, stderr) {
+		successPush(error, stdout, stderr);
+	});
+}
+
+exports.all = function(problemNumber, commitMessage, remoteUrl, repoPath, successAll) {
+	exports.add(problemNumber, repoPath, function(error, stdout, stderr) {
+		exports.commit(commitMessage, repoPath, function(error, stdout, stderr) {
+			exports.push(remoteUrl, repoPath, function(error, stdout, stderr) {
+				
+			});
+		});
+	});
+}
