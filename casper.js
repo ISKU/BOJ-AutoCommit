1800c9f8812fa8c3408cba37042e5532f6bdc302
Kim Min-Ho <minho1a@hanmail.net>
2017-02-07 01:05:32 +0000

    Add a function to find the problem solved by Casper.

---
+++ casper.js
@@ -0,0 +1,18 @@
+var command = (process.platform === 'win32') ? 'casperjs.cmd' : 'casperjs';
+
+exports.find = function(args, successFind) {
+	var spawn = require('child_process').spawn(command, args);
+	var info = new Object();
+
+	spawn.stdout.on('data', function(data) {
+		info = JSON.parse(data);
+	});
+
+	spawn.stderr.on('data', function(data) {
+		console.log(JSON.stringify(data));
+	});
+
+	spawn.on('exit', function(code) {
+		successFind(info);
+	});
+}
