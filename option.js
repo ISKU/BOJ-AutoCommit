15d39d6016d9992f9f53aa0d2116c72898b03c25
Kim Min-Ho <minho1a@hanmail.net>
2017-02-09 07:36:32 +0000

    Add ext verification code.

--- option.js
+++ option.js
@@ -53,7 +53,55 @@ exports.poll = function(userInfo) {
 
 exports.sourceName = function(problemNumber, language, userInfo) {
 	if (userInfo.hasOwnProperty('sourceName'))
-		return (userInfo.sourceName.replace(/\[NO\]/gi, problemNumber)) + '.' + language;
+		return (userInfo.sourceName.replace(/\[NO\]/gi, problemNumber)) + getExt(language);
 
-	return problemNumber + '.' + language;
+	return problemNumber + getExt(language);
+}
+
+function getExt(language) {
+	switch (language) {
+		case 'C': return '.c';
+		case 'C++': return '.cc';
+		case 'C++11': return '.cc';
+		case 'C++14': return '.cc';
+		case 'Java': return '.java';
+		case 'C11': return '.c';
+		case 'Python': return '.py';
+		case 'Python3': return '.py';
+		case 'PyPy': return '.py';
+		case 'PyPy3': return '.py';
+		case 'Ruby2.2': return '.rb';
+		case 'C#4.0': return '.cs';
+		case 'Text': return '.txt';
+		case 'Go': return '.go';
+		case 'F#': return '.fs';
+		case 'Pascal': return '.pas';
+		case 'Lua': return '.lua';
+		case 'Perl': return '.pl';
+		case 'C(Clang)': return '.c';
+		case 'C++11(Clang)': return '.cc';
+		case 'C++14(Clang)': return '.cc';
+		case 'Fortran': return '.f95';
+		case 'Scheme': return '.scm';
+		case 'Ada': return '.ada';
+		case 'awk': return '.awk';
+		case 'OCaml': return '.ml';
+		case 'Brainfuck': return '.bf';
+		case 'Whitespace': return '.ws';
+		case 'Tcl': return '.tcl';
+		case 'Assembly(32bit)': return '.asm';
+		case 'D': return '.d';
+		case 'Clojure': return '.clj';
+		case 'Rhino': return '.js';
+		case 'Cobol': return '.cob';
+		case 'SpiderMonkey': return '.js';
+		case 'Pike': return '.pike';
+		case 'sed': return '.sed';
+		case 'Rust': return '.rs';
+		case 'Intercal': return '.i';
+		case 'bc': return '.bc';
+		case 'VB.NET4.0': return '.vb';
+		case '아희': return '.aheui';
+		default: return '';
+	}
 }
